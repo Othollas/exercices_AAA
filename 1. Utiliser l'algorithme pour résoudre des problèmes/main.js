e771d005ba5a69c09e0db3a391ffe2ee40866c40
o_thollas <o_thollas@hotmail.com>
2025-05-14 01:54:00 +0200

    ajout d'un exercice

--- 1. Utiliser l'algorithme pour résoudre des problèmes/main.js
+++ 1. Utiliser l'algorithme pour résoudre des problèmes/main.js
@@ -23,3 +23,48 @@
             setInterval(()=> age_minor.classList.add("hidden"), 1000);
         }
     })
+
+    // exercice 2 
+
+    let chiffre_a_deviner = Math.floor(Math.random()*100);
+    console.log(chiffre_a_deviner);
+    let jouer = document.getElementById("jouer");
+    let rejouer = document.getElementById("rejouer");
+    let gagnant = document.getElementById("succes");
+    let perdu = document.getElementById("perdu");
+    let trop_bas = document.getElementById("bas");
+    let trop_haut = document.getElementById("haut");
+    let score = 3
+
+    jouer.addEventListener("click", ()=>{
+        let chiffre_utilisateur = document.getElementById("chiffre_utilisateur").value;
+        trop_bas.classList.add("hidden");
+        trop_haut.classList.add("hidden");
+        rejouer.classList.add("hidden");
+        
+        console.log(chiffre_a_deviner)
+        console.log(score)
+        if(chiffre_a_deviner>chiffre_utilisateur  && score !=0){
+            trop_bas.classList.remove("hidden");
+            score-=1;
+        }else if(chiffre_a_deviner<chiffre_utilisateur  && score !=0){
+            trop_haut.classList.remove("hidden");
+            score-=1;
+        }else if(chiffre_a_deviner == chiffre_utilisateur){
+            gagnant.classList.remove("hidden");
+            jouer.disabled = true;
+            rejouer.classList.remove("hidden");
+        }else if(score === 0){
+            perdu.classList.remove("hidden");
+            rejouer.classList.remove("hidden");
+        }
+    })
+
+    rejouer.addEventListener("click", ()=>{
+        chiffre_a_deviner = Math.floor(Math.random()*100);
+        rejouer.classList.add("hidden");
+        perdu.classList.add("hidden");
+        gagnant.classList.add("hidden");
+        score = 3;
+        jouer.disabled = false
+    })
